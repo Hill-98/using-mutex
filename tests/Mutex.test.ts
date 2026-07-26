6541b85221978a95226b7ed8311f8282cc897376
Zhong Lufan <lufanzhong@gmail.com>
2026-07-26 17:39:47 +0800

    test: better descriptions and logic

--- tests/Mutex.test.ts
+++ tests/Mutex.test.ts
@@ -5,7 +5,24 @@ import { Mutex } from '../src/Mutex.ts'
 
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
 
-test('Mutex test', async (t: TestContext) => {
+test('mutex simple test', async (t: TestContext) => {
+  const mutex = new Mutex()
+  let x = 0
+  const add = async function add() {
+    using _ = await mutex.acquire()
+    await sleep(100)
+    x += 1
+  }
+  add()
+  add()
+  await sleep(120)
+  t.assert.strictEqual(x, 1)
+  await sleep(120)
+  t.assert.strictEqual(x, 2)
+})
+
+test('mutex acquire timeout test', async (t: TestContext) => {
+  t.plan(3)
   const mutex = new Mutex()
   let x = 0
   const add = async function add(timeout?: number) {
@@ -14,42 +31,31 @@ test('Mutex test', async (t: TestContext) => {
     x += 1
   }
   add()
-  add()
-  await sleep(150)
-  t.assert.strictEqual(x, 1)
-  await sleep(150)
-  t.assert.strictEqual(x, 2)
-  add()
   try {
-    await add(10)
-    t.assert.ok(false)
+    await add(50)
   } catch (err: unknown) {
     t.assert.ok(err instanceof DOMException)
     t.assert.strictEqual(err.name, 'TimeoutError')
   }
-})
-
-test('two Mutex test', async (t: TestContext) => {
-  const mutex = new Mutex()
-  let [x, y] = [0, 0]
-  const addX = async function addX() {
-    using _ = await mutex.acquire('x')
-    await sleep(200)
-    x += 1
-  }
-  const addY = async function addY() {
-    using _ = await mutex.acquire('y')
-    await sleep(200)
-    y += 1
-  }
-  addX()
-  addY()
-  await sleep(250)
+  await sleep(70)
   t.assert.strictEqual(x, 1)
-  t.assert.strictEqual(y, 1)
 })
 
-test('Mutex abort signal test', async (t: TestContext) => {
+test('mutex different keys test', async (t: TestContext) => {
+  const mutex = new Mutex()
+  const keys: string[] = []
+  const add = async function add(key: string) {
+    using _ = await mutex.acquire(key)
+    await sleep(100)
+    keys.push(key)
+  }
+  add('x')
+  add('y')
+  await sleep(150)
+  t.assert.deepStrictEqual(keys, ['x', 'y'])
+})
+
+test('mutex abort signal test', async (t: TestContext) => {
   const mutex = new Mutex()
   let x = 0
   const add = async function add(signal?: AbortSignal) {
@@ -57,51 +63,48 @@ test('Mutex abort signal test', async (t: TestContext) => {
     await sleep(100)
     x += 1
   }
-  add()
 
-  try {
-    const controller = new AbortController()
-    controller.abort('test')
-    await add(controller.signal)
-  } catch (err: unknown) {
-    t.assert.strictEqual(err, 'test')
-  }
+  using _ = await mutex.acquire('signal')
 
-  try {
-    const controller = new AbortController()
-    controller.abort(null)
-    await add(controller.signal)
-  } catch (err: unknown) {
-    t.assert.ok(err instanceof Error)
-    t.assert.strictEqual(err.message, 'Abort')
-  }
+  await t.test('pass a aborted signal', async (t: TestContext) => {
+    t.plan(1)
+    try {
+      const controller = new AbortController()
+      controller.abort('test')
+      await add(controller.signal)
+    } catch (err: unknown) {
+      t.assert.strictEqual(err, 'test')
+    }
+  })
 
-  try {
-    const controller = new AbortController()
-    setTimeout(() => controller.abort(null), 10)
-    await add(controller.signal)
-  } catch (err: unknown) {
-    t.assert.ok(err instanceof Error)
-    t.assert.strictEqual(err.message, 'Abort')
-  }
+  await t.test('delay abort signal', async (t: TestContext) => {
+    t.plan(2)
+    try {
+      const controller = new AbortController()
+      setTimeout(() => controller.abort(), 10)
+      await add(controller.signal)
+    } catch (err: unknown) {
+      t.assert.ok(err instanceof DOMException)
+      t.assert.strictEqual(err.message, 'This operation was aborted')
+    }
+  })
 
   await sleep(100)
-  t.assert.strictEqual(x, 1)
+  t.assert.strictEqual(x, 0)
 })
 
-test('Mutex error test', async (t: TestContext) => {
+test('mutex.acquire args throw error test', async (t: TestContext) => {
+  t.plan(4)
   const mutex = new Mutex()
   using _ = await mutex.acquire()
   try {
     await mutex.acquire(0)
-    t.assert.ok(false)
   } catch (err: unknown) {
     t.assert.ok(err instanceof MutexAcquireError)
     t.assert.strictEqual(err.message, 'Mutex is in use and cannot be acquired as there is no timeout.')
   }
   try {
     await mutex.acquire(-1)
-    t.assert.ok(false)
   } catch (err: unknown) {
     t.assert.ok(err instanceof RangeError)
     t.assert.strictEqual(err.message, 'timeout must be a positive number.')
@@ -122,14 +125,14 @@ test('Mutex many test', async (t: TestContext) => {
   t.assert.strictEqual(x, 2)
 })
 
-test('MutexGuard key test ', async (t: TestContext) => {
+test('mutex key test ', async (t: TestContext) => {
   const mutex = new Mutex()
   const symbol = Symbol()
   using lock = await mutex.acquire(symbol)
   t.assert.strictEqual(lock.key, symbol)
 })
 
-test('MutexGuard multiple release test ', async (t: TestContext) => {
+test('mutex multiple release test ', async (t: TestContext) => {
   const mutex = new Mutex()
   let x = 0
   const add = async function add() {
